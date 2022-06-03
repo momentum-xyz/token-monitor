fa087443011da90cfc9f511db04136b6fbad683b
Wasiq Nazir <wasiq@unchain.io>
2022-06-03 12:12:21 +0200

    no message

--- pkg/types/name_request.go
+++ pkg/types/name_request.go
@@ -5,6 +5,6 @@ import "math/big"
 type NameRequest struct {
 	ContractAddress string   `json:"contractAddress" validate:"required"`
 	TokenID         *big.Int `json:"tokenId"`
-	TokenType       string   `json:"tokenType" validate:"required"`
+	TokenType       string   `json:"tokenType" validate:"required" enums:"erc20,erc721,erc1155"`
 	Network         Network  `json:"network"`
 }
